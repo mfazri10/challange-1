1a02d4cf74421b616fde71a05c1a51b15a2e17ca
Muhammad Fazri <muhammadfazri10@gmail.com>
2023-10-06 23:40:13 +0700

    feat : add to main

--- README.md
+++
@@ -1,4 +0,0 @@
-# challange-1
-Challange BINAR ACADEMY ke -1
-
-membuat
--- bank_account.js
+++ bank_account.js
@@ -1,35 +1,54 @@
-const
-    tambah = document.getElementById('tambah'),
-    tarik = document.getElementById('tarik'),
-    saldo = document.getElementById('saldo'),
-    tambahInput = document.getElementById('tambah-input'),
-    tarikInput = document.getElementById('tarik-input'),
-    tambahBtn = document.getElementById('tambah-btn'),
-    tarikBtn = document.getElementById('tarik-btn'),
-    submitBtn = document.getElementById('submit-btn');
-
-//Menambahkan dana
-tambahBtn.addEventListener('click', () => {
-    const value = tambahInput.value;
-    const tambahValue = Number(tambah.innerText) + Number(value);
-    const saldoValue = Number(saldo.innerText) + Number(value);
-    tambah.innerText = tambahValue;
-    saldo.innerText = saldoValue;
-    tambahInput.value = '';
-})
-
-//Tarik dana
-tarikBtn.addEventListener('click', () => {
-    const value = tarikInput.value;
-    if (Number(value) === 0) {
-        alert("Masukan jumlah penarikan!!!");
-    } else if (Number(value) > Number(saldo.innerText)) {
-        alert("Saldo anda tidak Cukup!!!");
-    } else {
-        const saldoValue = Number(saldo.innerText) - Number(value);
-        const tarikValue = Number(tarik.innerText) + Number(value);
-        tarik.innerText = tarikValue;
-        saldo.innerText = saldoValue;
-        tarikInput.value = '';
+class Bank {
+    constructor() {
+        this.tambah = document.getElementById('tambah');
+        this.tarik = document.getElementById('tarik');
+        this.saldo = document.getElementById('saldo');
+        this.tambahInput = document.getElementById('tambah-input');
+        this.tarikInput = document.getElementById('tarik-input');
+        this.tambahBtn = document.getElementById('tambah-btn');
+        this.tarikBtn = document.getElementById('tarik-btn');
+        this.submitBtn = document.getElementById('submit-btn');
     }
-})
+
+    async tambahDana() {
+        this.tambahBtn.addEventListener('click', async () => {
+            const value = this.tambahInput.value;
+            await this.setTimeoutPromise(1000);
+            const tambahValue = Number(this.tambah.innerText) + Number(value);
+            const saldoValue = Number(this.saldo.innerText) + Number(value);
+            this.tambah.innerText = tambahValue;
+            this.saldo.innerText = saldoValue;
+            this.tambahInput.value = '';
+        });
+    }
+
+    async tarikDana() {
+        this.tarikBtn.addEventListener('click', async () => {
+            const value = this.tarikInput.value;
+            await this.setTimeoutPromise(1000);
+            if (Number(value) === 0) {
+                alert("Masukan jumlah penarikan!!!");
+            } else if (Number(value) > Number(this.saldo.innerText)) {
+                alert("Saldo anda tidak Cukup!!!");
+            } else {
+                const saldoValue = Number(this.saldo.innerText) - Number(value);
+                const tarikValue = Number(this.tarik.innerText) + Number(value);
+                this.tarik.innerText = tarikValue;
+                this.saldo.innerText = saldoValue;
+                this.tarikInput.value = '';
+            }
+        });
+    }
+
+    setTimeoutPromise(delay) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve();
+            }, delay);
+        });
+    }
+}
+
+const bank = new Bank();
+bank.tambahDana();
+bank.tarikDana();
